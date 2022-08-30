a497bec9c071469bde464e28ec2f0b40d2207415
CerBor <58470353+CerBor@users.noreply.github.com>
2022-08-30 16:39:45 +0700

    Opponent Play

--- source/GameplayChangersSubstate.hx
+++ source/GameplayChangersSubstate.hx
@@ -90,6 +90,9 @@ class GameplayChangersSubstate extends MusicBeatSubstate
 
 		var option:GameplayOption = new GameplayOption('Botplay', 'botplay', 'bool', false);
 		optionsArray.push(option);
+		
+		var option:GameplayOption = new GameplayOption('Plas As Opponent', 'opponentplay', 'bool', false);
+		optionsArray.push(option);
 	}
 
 	public function getOptionByName(name:String)
